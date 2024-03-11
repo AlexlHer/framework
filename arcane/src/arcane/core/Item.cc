cf276b3d442ffa224a1ff4cfc8573a10e64ad261
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2024-03-11 18:56:48 +0100

    Test rebase

--- arcane/src/arcane/core/Item.cc
+++ arcane/src/arcane/core/Item.cc
@@ -1,4 +1,4 @@
-﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
 //-----------------------------------------------------------------------------
 // Copyright 2000-2023 CEA (www.cea.fr) IFPEN (www.ifpenergiesnouvelles.com)
 // See the top-level COPYRIGHT file for details.
@@ -53,6 +53,7 @@ typeName(Integer t)
   return ItemTypeMng::_legacyTypeName(t);
 }
 
+////
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
